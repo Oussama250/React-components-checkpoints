9d0e9f787e90c52142802652f58417a3779ca329
Oussama Beddiaf <oussama-mias@live.fr>
2021-05-22 09:53:48 +0100

    added components.

---
+++ components/src/Components/Profile/Address.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Address = () => {
+    return (
+        <p>Constantine 2500, Algeria.</p>
+    );
+}
+
+export default Address;
---
+++ components/src/Components/Profile/ProfilePhoto.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import user from './user.png';
+
+const Photo = () => {
+    return (
+        <img src={user} alt='user.png'/>
+    );
+}
+
+export default Photo;
